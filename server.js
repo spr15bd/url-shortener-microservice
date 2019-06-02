c10147040bd552f8a0e071ca7270195875b54e70
spr15bd <bevandady@gmail.com>
2019-06-02 21:43:29 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ var dns = require('dns');
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
+
+
+app.use(cors());
 /** this project needs a db !! **/ 
 mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true });
 var Schema = mongoose.Schema;
@@ -22,23 +25,25 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function() {
   console.log('Connected to MongoDB');
+  
 });
 
-
-app.use(cors());
 
 var webAddressSchema = new Schema({
   url: {
     type: String,
-    required: true
+    required: true,
+    unique:true
   },
   shortUrl: {
-    type: Number,
-    required: true
+    type: Number
+    //required: true
   }
 });
 
-var Address = mongoose.model('Address', webAddressSchema); 
+
+
+var address = mongoose.model('address', webAddressSchema); 
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
@@ -61,21 +66,28 @@ app.post("/api/shorturl/new", function (req, res) {
     }
     
     
-    var createAndSaveWebAddress = function(done){
-      var addr = new Address({url: url, shortUrl: 1});
-      console.log("new address created: "+addr);
+    var createAndSaveWebAddress = function(){
+      var addr = new address({url: url});
+      
       addr.save(function(err, data){
         if(err) {
-          done(err);
+          console.log("There is an error: "+err);
+          
         }
         else {
-          done(null, data);
+          
+          console.log("new address created in db: "+addr);
+          console.log("sending web addr in json");
+          // TODO - return json with retrieved _id for the website
+          res.json({
+            original_url: req.body.url,
+            short_url: addr._id
+          });
         }
       });
     };
-    console.log("sending web addr in json");
-    res.json({original_url: req.body.url});
     
+    createAndSaveWebAddress();
     
   });
   //res.json({original_url: 'hello API'});
